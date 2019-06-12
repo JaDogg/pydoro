46d17e1fef2126618e2ebb5cabb4551f3646f23d
JaDogg <jadogg.coder@gmail.com>
2019-06-12 21:34:49 +0100

    Install pyobjc for OSX
    Signed-off-by: JaDogg <jadogg.coder@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from codecs import open
 from inspect import getsource
 from os.path import abspath, dirname, join
+from platform import system
 
 from setuptools import setup, find_packages
 
 here = abspath(dirname(getsource(lambda: 0)))
+system = system()
 
 with open(join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
+
+requires = ["prompt-toolkit>=2.0.9"]
+# WHY:
+# On OSX we need pyobjc to actually play a sound
+if system == "Darwin":
+    requires.extend(["pyobjc-core>=5.2", "pyobjc-framework-Cocoa>=5.2"])
 
 setup(
     name="pydoro",
@@ -20,7 +28,7 @@ setup(
     license="MIT",
     scripts=["pydoro.py"],
     package_data={"": ["*.rst"], "pydoro_util": ["b15.wav"]},
-    install_requires=["prompt-toolkit>=2.0.9"],
+    install_requires=requires,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
