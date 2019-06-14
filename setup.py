5b640071f9feffdea23cb9b993a36ec14fff8a14
JaDogg <jadogg.coder@gmail.com>
2019-06-15 00:54:14 +0100

    Fix audio requires
    Signed-off-by: JaDogg <jadogg.coder@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from codecs import open
 from inspect import getsource
 from os.path import abspath, dirname, join
+from platform import system
 
 from setuptools import setup, find_packages
 
@@ -9,9 +10,21 @@ here = abspath(dirname(getsource(lambda: 0)))
 with open(join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
+
+current_system = system()
+
+
+# WHY: Windows can be handled without extras
+audio_requires = []
+if current_system == "Linux":
+    audio_requires = ["pyobjc-core>=5.2", "pyobjc-framework-Cocoa>=5.2"]
+elif current_system == "Darwin":
+    audio_requires = ["pycairo>=1.18.1", "PyGObject>=3.32.1"]
+
 setup(
     name="pydoro",
     version="0.1.0",
+    # Get the description from second line & remove `*` character
     description=long_description.splitlines()[2][1:-1],
     long_description=long_description,
     url="https://github.com/JaDogg/pydoro",
@@ -20,14 +33,7 @@ setup(
     license="MIT",
     package_data={"": ["*.rst", "*.wav"]},
     install_requires=["prompt-toolkit>=2.0.9"],
-    extras_require={
-        "audio": [
-            'pyobjc-core>=5.2;platform_system="Darwin"',
-            'pyobjc-framework-Cocoa>=5.2;platform_system="Darwin"',
-            'pycairo>=1.18.1;platform_system="Linux"',
-            'PyGObject>=3.32.1;platform_system="Linux"',
-        ]
-    },
+    extras_require={"audio": audio_requires},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Information Technology",
