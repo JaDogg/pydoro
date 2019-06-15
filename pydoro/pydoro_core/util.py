cbcae42675f94e86ea2a81fc5a6e5c99b4e73577
JaDogg <jadogg.coder@gmail.com>
2019-06-15 02:20:05 +0100

    Pyinstaller bundle .wav file
    Signed-off-by: JaDogg <jadogg.coder@gmail.com>

--- pydoro/pydoro_core/util.py
+++ pydoro/pydoro_core/util.py
@@ -1,27 +1,29 @@
-import time
 import os
+import time
 
 
 def every(delay, task):
     next_time = time.time() + delay
     while True:
         time.sleep(max(0, next_time - time.time()))
-        try:
-            task()
-        except Exception:
-            pass
-            # in production code you might want to have this instead of course:
-            # logger.exception("Problem while executing repetitive task.")
-        # skip tasks if we are behind schedule:
+        task()
         next_time += (time.time() - next_time) // delay * delay + delay
 
 
 def in_app_path(path):
+    import sys
+
+    try:
+        wd = sys._MEIPASS
+        return os.path.abspath(os.path.join(wd, path))
+    except AttributeError:
+        return _from_resource(path)
+
+
+def _from_resource(path):
     from pkg_resources import resource_filename
 
     res_path = resource_filename(__name__, path)
-
     if not os.path.exists(res_path):
         res_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-
     return res_path
