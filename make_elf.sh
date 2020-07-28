cab369b1c2fd3a52ec64f493278f4737e8e0edbd
JaDogg <JaDogg@users.noreply.github.com>
2020-07-28 21:06:15 +0100

    chore: Rename make_elf to make_unix
    Signed-off-by: JaDogg <JaDogg@users.noreply.github.com>

--- make_elf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pyinstaller -F pydoro/pydoro_tui.py --add-data "./pydoro/pydoro_core/b15.wav:." --add-data "./.venv/lib/python3.7/site-packages/wcwidth:wcwidth" --hidden-import="pkg_resources.py2_warn"
-
