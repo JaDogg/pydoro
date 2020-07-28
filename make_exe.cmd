be6621ec6303f1207499e7217b53aef4fabe8b27
JaDogg <JaDogg@users.noreply.github.com>
2020-07-28 21:05:57 +0100

    chore: Use pyinstaller arg -n to rename output binary
    Signed-off-by: JaDogg <JaDogg@users.noreply.github.com>

--- make_exe.cmd
+++ make_exe.cmd
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller pydoro\pydoro_tui.py --onefile --icon images\Tomato.ico --add-data ".\pydoro\pydoro_core\b15.wav;." --add-data ".\.venv\Lib\site-packages\wcwidth;wcwidth" --hidden-import="pkg_resources.py2_warn"
+pyinstaller pydoro\pydoro_tui.py -n pydoro --onefile --icon images\Tomato.ico --add-data ".\pydoro\pydoro_core\b15.wav;." --add-data ".\.venv\Lib\site-packages\wcwidth;wcwidth" --hidden-import="pkg_resources.py2_warn"
 
